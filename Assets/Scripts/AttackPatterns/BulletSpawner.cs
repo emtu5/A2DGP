77769d3c7dd1941078109f1e734c23f3a7476747
emtu5 <115076076+emtu5@users.noreply.github.com>
2026-05-17 02:41:10 +0300

    attack tweaks

--- Assets/Scripts/AttackPatterns/BulletSpawner.cs
+++ Assets/Scripts/AttackPatterns/BulletSpawner.cs
@@ -45,7 +45,7 @@ public class BulletSpawner : MonoBehaviour
 
             // Bullet newBullet = Instantiate(bulletPrefab).GetComponent<Bullet>();
 
-            float spawnOffset = 0.6f;
+            float spawnOffset = 0.4f;
             GameObject bulletObj = bulletPool.GetPooledObject().gameObject;
             Bullet newBullet = bulletObj.GetComponent<Bullet>();
             newBullet.transform.position = startPoint + bulletDirection * spawnOffset;
