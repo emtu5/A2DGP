2f72a9d25544ae495ae123babee19a79acca4bc8
emtu5 <115076076+emtu5@users.noreply.github.com>
2026-05-24 04:21:37 +0300

    first version of target arrow pointing to enemy

---
+++ Assets/Scripts/Player/TargetArrow.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class TargetArrow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
