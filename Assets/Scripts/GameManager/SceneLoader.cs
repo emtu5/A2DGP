7a81d3e7f0be0f30a7788f5d562f1f1a98210967
emtu5 <115076076+emtu5@users.noreply.github.com>
2026-06-03 04:21:13 +0300

    assigning attacks + win/loss scenes

--- Assets/Scripts/GameManager/SceneLoader.cs
+++ Assets/Scripts/GameManager/SceneLoader.cs
@@ -1,13 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] private string sceneToLoad;
     void OnEnable()
     {
         GameEvents.OnPlayPressed += LoadGame;
         GameEvents.OnQuitPressed += QuitGame;
         GameEvents.OnMainMenu += MainMenu;
+        Time.timeScale = 1f;
     }
 
     void OnDisable()
@@ -19,7 +22,7 @@ public class SceneLoader : MonoBehaviour
 
     void LoadGame()
     {
-        SceneManager.LoadScene("ArenaTest");
+        SceneManager.LoadScene(sceneToLoad);
     }
 
     void QuitGame()
